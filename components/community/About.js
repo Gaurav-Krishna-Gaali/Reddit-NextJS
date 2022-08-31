7a94bb0604fa1a4293a44ef2a1d5e19d5dc606b0
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-08-31 19:05:25 +0530

    Cleanup 🧹

--- components/community/About.js
+++ components/community/About.js
@@ -1,32 +1,32 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 const style = {
   wrapper:
-    'flex flex-col space-y-2 divide-y divide-[#343536] rounded border border-[#343536] bg-[#1a1a1b] p-4 text-gray-300',
-  profileInfoContainer: 'flex items-center space-x-4 ',
+    "flex flex-col space-y-2 divide-y divide-[#343536] rounded border border-[#343536] bg-[#1a1a1b] p-4 text-gray-300",
+  profileInfoContainer: "flex items-center space-x-4 ",
   profilePicContainer: `relative h-12 w-12`,
-  profilePic: 'object-contain',
-  aboutContent: 'py-2 text-sm',
-  statsWrapper: 'flex items-center space-x-16',
-  stat: 'flex flex-col',
-  statTitle: 'text-xs',
-  footer: 'flex flex-col space-y-4 pt-2',
-  createdAt: 'text-sm font-light',
+  profilePic: "object-contain",
+  aboutContent: "py-2 text-sm",
+  statsWrapper: "flex items-center space-x-16",
+  stat: "flex flex-col",
+  statTitle: "text-xs",
+  footer: "flex flex-col space-y-4 pt-2",
+  createdAt: "text-sm font-light",
   joinedButton:
-    'cursor-pointer rounded-full border border-gray-300 py-1 text-center text-sm font-semibold',
-}
+    "cursor-pointer rounded-full border border-gray-300 py-1 text-center text-sm font-semibold",
+};
 
 const About = () => {
   return (
     <div className={style.wrapper}>
-      <div className='pb-2'>
+      <div className="pb-2">
         <div className={style.profileInfoContainer}>
           <div className={style.profilePicContainer}>
             <Image
-              src='https://i.ibb.co/Ks6r2s5/r-Ll-Cifh-XRJi-T0-Ro-N2-Fj-K-Logo-roundbackground-black.png'
-              layout='fill'
+              src="https://i.ibb.co/Ks6r2s5/r-Ll-Cifh-XRJi-T0-Ro-N2-Fj-K-Logo-roundbackground-black.png"
+              layout="fill"
               className={style.profilePic}
-              alt='Profile Image'
+              alt="Profile Image"
             />
           </div>
           <p>r/cleverprogrammer</p>
@@ -37,7 +37,7 @@ const About = () => {
           reality with Programming. We speak business and code fluently. Here at
           Reddit, you can find us sharing our coding war stories around a
           fireplace. So, grab a cup of coffee and join us. We got lots to catch
-          up on… You'll find awesome programming lessons here! Also, expect
+          up on… Youll find awesome programming lessons here! Also, expect
           programming tips and tricks that will take your coding skills to the
           next level.
         </p>
@@ -60,7 +60,7 @@ const About = () => {
         <div className={style.joinedButton}>Joined</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
